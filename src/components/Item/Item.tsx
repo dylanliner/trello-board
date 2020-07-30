5e47ed30924dd84b848eeadcd8ad8f4cfa1d332d
cap3k <37514740+cap3k@users.noreply.github.com>
2020-07-31 00:44:05 +0200

    WIP moving items from column to column

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,31 +1,84 @@
-import React from 'react';
-import styles from './Item.module.scss';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from "react";
+import styles from "./Item.module.scss";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { prototype } from "events";
+
+export enum Status{
+NEW,
+TODO,
+UNDER_DEV,
+TO_DELIVER,
+TESTING,
+DONE
+}
 
 export interface ItemProps {
+  id: string;
   name: string;
   description: string;
+  status: Status;
+  moveToNextColumn: (item: ItemProps) => void;
+ 
 }
-const Item: React.FC<ItemProps> = (props: ItemProps) => (
-  <div className={styles.Item}>
-     <Card>
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          {props.name}
-        </Typography>
-        <Typography variant="h5" component="h2">
-        {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Save</Button>
-      </CardActions>
-    </Card>
-  </div>
-);
+const Item: React.FC<ItemProps> = (props: ItemProps) => {
+  const [disabled, setDisabled] = useState(props.status !== Status.TODO);
+  const [form, setForm] = useState<ItemProps>({...props});
 
+  const handleSubmit = (event: any) => {
+    form.id=form.name;
+    console.log(JSON.stringify(form));
+    setDisabled(true);
+    fetch("https://your-node-server-here.com/api/endpoint", {
+      method: "POST",
+      // We convert the React state to JSON and send it as the POST body
+      body: JSON.stringify(props),
+    }).then(function (response) {
+      console.log(response);
+      return response.json();
+    });
+  };
+
+  return (
+    <div className={styles.Item}>
+      <Card>
+        <CardContent>
+          <form autoComplete="off">
+            <TextField
+              disabled={disabled}
+              id="standard-basic"
+              label="Name of task"
+              value={props.name}
+              onChange={(e) => (form.name = e.target.value)}
+            />
+
+            <TextField
+              id="standard-multiline-static"
+              label="Description"
+              multiline
+              disabled={disabled}
+              value={props.description}
+              rows={4}
+              onChange={(e) => (form.description = e.target.value)}
+            />
+          </form>
+        </CardContent>
+        <CardActions>
+          {props.status === Status.TODO && !disabled? (
+            <Button onClick={handleSubmit} size="small">
+              Save
+            </Button>
+          ) : (
+            <Button onClick={()=>{props.moveToNextColumn(form)}} size="small">
+              Move to next column
+            </Button>
+          )}
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 export default Item;
--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -3,53 +3,167 @@ import styles from "./Board.module.scss";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import Item, { ItemProps } from '../Item/Item';
+import Item, { ItemProps, Status } from "../Item/Item";
 
 const Board: React.FC = () => {
-
   const [newTasks, setNewTasks] = useState<ItemProps[]>([]);
- 
+  const [tasksUnderDev, setTasksUnderDev] = useState<ItemProps[]>([]);
+  const [tasksToTest, setTasksToTest] = useState<ItemProps[]>([]);
+  const [tasksToDeliver, setTasksToDeliver] = useState<ItemProps[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<ItemProps[]>([]);
 
-  const createTask= ()=>{ 
+  const createTask = () => {
     const item = {} as ItemProps;
-    item.name ="Test";
-    item.description="This is a test";
-    newTasks.push(item);
-    setNewTasks([...newTasks]);
-  }
+    item.status = Status.TODO;
+    item.moveToNextColumn = moveToNextColumn;
+    setNewTasks([...newTasks, item]);
+  };
 
-  return(
-  <div className={styles.Board}>
-    <Grid spacing={1} container direction="row" justify="space-evenly">
-      <Grid item xs={2}>
-        <Paper className={styles.paper}>
-          <Grid spacing={1} container direction="column" justify="space-evenly">
-            <Grid item>To Do</Grid>
-            {newTasks.map((itemProps: ItemProps, index: number) => (
-             <Grid item>
-              <Item key={index.toString()} {...itemProps}/>
+  const moveToNextColumn = (item: ItemProps) => {
+    switch (item.status) {
+      case Status.TODO: {
+        item.status = Status.UNDER_DEV;
+        setTasksUnderDev((newTasks) => [...newTasks, item]);
+        setNewTasks((newTasks) =>
+          newTasks.filter((newItem) => newItem.id !== item.id)
+        );
+        break;
+      }
+      case Status.UNDER_DEV: {
+        item.status = Status.TO_DELIVER;
+        setTasksToDeliver((tasks) => [...tasks, item]);
+        setTasksUnderDev((newTasks) =>
+          newTasks.filter((newItem) => newItem.id !== item.id)
+        );
+        break;
+      }
+      case Status.TO_DELIVER: {
+        item.status = Status.TESTING;
+        setTasksToTest((tasks) => [...tasks, item]);
+        setTasksToDeliver((newTasks) =>
+          newTasks.filter((newItem) => newItem.id !== item.id)
+        );
+        break;
+      }
+      case Status.TESTING: {
+        item.status = Status.DONE;
+        setCompletedTasks((tasks) => [...tasks, item]);
+        setTasksToTest((newTasks) =>
+          newTasks.filter((newItem) => newItem.id !== item.id)
+        );
+        break;
+      }
+      case Status.DONE: {
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className={styles.Board}>
+      <Grid spacing={1} container direction="row" justify="space-evenly">
+        <Grid item xs={2}>
+          <Paper className={styles.paper}>
+            <Grid
+              spacing={1}
+              container
+              direction="column"
+              justify="space-evenly"
+            >
+              <Grid item>To Do</Grid>
+              {newTasks.map((itemProps: ItemProps, index: number) => (
+                <div key={index.toString()}>
+                  <Grid item>
+                    <Item {...itemProps} />
+                  </Grid>
+                </div>
+              ))}
+              <Grid item>
+                <Button variant="contained" onClick={createTask}>
+                  Create Task
+                </Button>
               </Grid>
-            ))}
-            <Grid item>
-              <Button variant="contained" onClick={createTask}>Create Task</Button>
             </Grid>
-          </Grid>
-        </Paper>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={styles.paper}>
+            <Grid
+              spacing={1}
+              container
+              direction="column"
+              justify="space-evenly"
+            >
+              <Grid item>Under Development</Grid>
+              {tasksUnderDev.map((itemProps: ItemProps, index: number) => (
+                <div key={index.toString()}>
+                  <Grid item>
+                    <Item {...itemProps} />
+                  </Grid>
+                </div>
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={styles.paper}>
+            <Grid
+              spacing={1}
+              container
+              direction="column"
+              justify="space-evenly"
+            >
+              <Grid item>To Deliver</Grid>
+              {tasksToDeliver.map((itemProps: ItemProps, index: number) => (
+                <div key={index.toString()}>
+                  <Grid item>
+                    <Item {...itemProps} />
+                  </Grid>
+                </div>
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={styles.paper}>
+            <Grid
+              spacing={1}
+              container
+              direction="column"
+              justify="space-evenly"
+            >
+              <Grid item>Testing</Grid>
+              {tasksToTest.map((itemProps: ItemProps, index: number) => (
+                <div key={index.toString()}>
+                  <Grid item>
+                    <Item {...itemProps} />
+                  </Grid>
+                </div>
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={styles.paper}>
+            <Grid
+              spacing={1}
+              container
+              direction="column"
+              justify="space-evenly"
+            >
+              <Grid item>Done</Grid>
+              {completedTasks.map((itemProps: ItemProps, index: number) => (
+                <div key={index.toString()}>
+                  <Grid item>
+                    <Item {...itemProps} />
+                  </Grid>
+                </div>
+              ))}
+            </Grid>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={2}>
-        <Paper className={styles.paper}>Under Development</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={styles.paper}>To Deliver</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={styles.paper}>Testing</Paper>
-      </Grid>
-      <Grid item xs={2}>
-        <Paper className={styles.paper}>Done</Paper>
-      </Grid>
-    </Grid>
-  </div>
-);
+    </div>
+  );
 };
 export default Board;
